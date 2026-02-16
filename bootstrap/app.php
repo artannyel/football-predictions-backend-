2341a13676d83d91e58691a433f1cf623063ebfe
Francisco Artannyel Pinto Ferreira <=>
2026-02-16 12:20:55 -0300

    feat: add endpoints for admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AuthenticateFirebaseToken;
+use App\Http\Middleware\CheckAdminKey;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'auth.firebase' => AuthenticateFirebaseToken::class,
+            'auth.admin' => CheckAdminKey::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
